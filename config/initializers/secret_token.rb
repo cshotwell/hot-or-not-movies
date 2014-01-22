c85f7255804e81787a54bb66207aa022e023e5ab
Carl Shotwell <carl.shotwell@leandog.com>
2014-01-22 09:35:45 -0500

    railsifying things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HotOrNotMovies::Application.config.secret_key_base = 'a8d81d912de8ccde6e01236114450a8f01393560dd0e68763cce870ec08a859324d1cfa2005ef54f852f02506c7c5404214ca3da96cc77561e2fbb1a6eac0eae'
