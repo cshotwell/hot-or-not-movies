26d907faff055470ed1c3cdebe7bdd57be7d4542
Carl Shotwell <carl.shotwell@leandog.com>
2014-01-24 00:08:19 -0500

    generating step definitions

---
+++ features/step_definitions/leaderboard_steps.rb
@@ -0,0 +1,13 @@
+Given(/^"(.*?)" has (\d+) votes$/) do |arg1, arg2|
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I view the leaderboard$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the movies in order:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  #   pending # express the regexp above with the code you wish you had
+  #   end
+end
